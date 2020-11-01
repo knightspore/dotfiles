e1c9c29740dcfc7ebe5317653ec7bff4e1425cef
Knightspore <cslemon96@gmail.com>
2020-11-01 10:56:57 +0200

    Updated settings for i3 manjaro, added Xresources

--- .config/fish/functions/dev.fish
+++ .config/fish/functions/dev.fish
@@ -1,4 +1,4 @@
 # Defined in - @ line 1
-function dev --wraps='cd /home/knightspore/Desktop/dev' --wraps='cd /home/knightspore/Desktop/dev && clear && ls -a -l' --wraps='cd /home/knightspore/Desktop/dev; clear; ls -a -l' --wraps='cd /home/knightspore/Desktop/dev; l;' --wraps='cd /home/knightspore/Desktop/dev; clear; l' --description 'alias dev=cd /home/knightspore/Desktop/dev; clear; l'
-  cd /home/knightspore/Desktop/dev; clear; l $argv;
+function dev --wraps='cd /home/knightspore/Desktop/dev' --wraps='cd /home/knightspore/Desktop/dev && clear && ls -a -l' --wraps='cd /home/knightspore/Desktop/dev; clear; ls -a -l' --wraps='cd /home/knightspore/Desktop/dev; l;' --wraps='cd /home/knightspore/Desktop/dev; clear; l' --wraps='cd ~/Desktop/dev; clear; l' --description 'alias dev=cd ~/Desktop/dev; clear; l'
+  cd ~/Desktop/dev; clear; l $argv;
 end
