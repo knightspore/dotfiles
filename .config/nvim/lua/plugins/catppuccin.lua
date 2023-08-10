3384bde3337f6a25bf38b8945473688d798eb83d
Ciaran <cslemon96@gmail.com>
2023-08-10 12:11:22 +0200

    add catppuccin nvim

---
+++ .config/nvim/lua/plugins/catppuccin.lua
@@ -0,0 +1,13 @@
+require("catppuccin").setup({
+    flavour = "mocha",
+    transparent_background = true,
+    styles = {
+        comments = { "italic" },
+    },
+    integrations = {
+        mason = true,
+    },
+})
+vim.cmd.colorscheme "catppuccin"
+vim.g.airline_powerline_fonts = 1
+vim.g.airline_theme = 'catppuccin'
