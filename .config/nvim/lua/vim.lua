ceacabc56d1a108c76072a2bfed364ac9acf14a6
Ciaran <cslemon96@gmail.com>
2023-08-30 22:56:00 +0200

    Mac Updates

--- .config/nvim/lua/vim.lua
+++ .config/nvim/lua/vim.lua
@@ -13,7 +13,7 @@ vim.opt.smartindent = true
 vim.opt.relativenumber = true
 vim.opt.nu = true
 
-vim.opt.undodir = '/Users/c/.vim/undodir'
+vim.opt.undodir = os.getenv("HOME") .. '/.vim/undodir'
 vim.opt.undofile = true
 
 vim.opt.scrolloff = 12
