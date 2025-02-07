fe24a75d80f469ab4c0493674dc87623677f018e
Ciarán <ciaran@tripco.africa>
2025-02-07 17:53:58 +0200

    feat(nvim): new plugins, lsp improvements for work, recent files shortcut

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -5,17 +5,19 @@ return require('packer').startup(function(use)
         'VonHeikemen/lsp-zero.nvim',
         branch = 'v2.x',
         requires = {
+            -- LSP Support
             { 'neovim/nvim-lspconfig' },
-            {
-                'williamboman/mason.nvim',
-                opts = {
-                    ensure_installed = { "gopls" }
-                }
-            },
+            { 'williamboman/mason.nvim', opts = { ensure_installed = { "gopls" } } },
             { 'williamboman/mason-lspconfig.nvim' },
+            -- Autocompletion
             { 'hrsh7th/nvim-cmp' },
+            { 'hrsh7th/cmp-buffer' },
+            { 'hrsh7th/cmp-path' },
+            { 'saadparwaiz1/cmp_luasnip' },
             { 'hrsh7th/cmp-nvim-lsp' },
+            { 'hrsh7th/cmp-nvim-lua' },
             { 'L3MON4D3/LuaSnip' },
+            { 'rafamadriz/friendly-snippets' },
         }
     }
 
--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -4,7 +4,35 @@ lsp_zero.on_attach(function(client, bufnr)
     lsp_zero.default_keymaps({ buffer = bufnr })
     local opts = { buffer = bufnr }
     vim.keymap.set({ "n", "x" }, "<leader>p", function()
-        vim.lsp.buf.format({ async = false, timeout_ms = 10000 })
+        -- Get file type
+        local ft = vim.bo.filetype
+        local filepath = vim.fn.expand("%:p")
+
+        -- Custom Formatter Commands
+        local commands = {
+            php = function()
+                if filepath:find("/services/travel/lavender/") then
+                    -- Laravel - Pint
+                    local config = os.getenv("HOME") ..
+                        "/Developer/stack/services/travel/lavender/pint.json"
+                    vim.cmd("!pint --config=" .. config .. " " .. filepath)
+                elseif filepath:find("/services/travel/") then
+                    -- Travel - PHP-CS-Fixer
+                    local config = os.getenv("HOME") ..
+                        "/Developer/stack/utilities/php-cs-fixer/.php-cs-fixer.dist.php"
+                    vim.cmd("!php-cs-fixer fix --config=" .. config .. " " .. filepath)
+                end
+            end,
+            json = function()
+                vim.cmd("!jq . " .. filepath .. " > " .. filepath)
+            end,
+        }
+
+        if commands[ft] then
+            commands[ft]()
+        else
+            vim.lsp.buf.format({ async = false, timeout_ms = 10000 })
+        end
     end, opts)
 end)
 
@@ -19,7 +47,6 @@ require('lspconfig').lua_ls.setup({})
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
-    ensure_installed = {},
     handlers = {
         function(server_name)
             require('lspconfig')[server_name].setup({})
@@ -33,12 +60,13 @@ require('lspconfig').phpactor.setup {
     root_dir = require('lspconfig/util').root_pattern("composer.json", ".git"),
     init_options = {
         ["language_server_worse_reflection.inlay_hints.enable"] = true,
-        ["language_server_worse_reflection.inlay_hints.params"] = true,
         ["language_server_worse_reflection.inlay_hints.types"] = true,
         ["language_server_configuration.auto_config"] = false,
         ["code_transform.import_globals"] = true,
-        ["language_server_phpstan.enabled"] = true,
-        ["language_server_phpstan.bin"] = "phpstan",
+        ["language_server_php_cs_fixer.enabled"] = true,
+        ["language_server_php_cs_fixer.bin"] = "/opt/homebrew/bin/php-cs-fixer",
+        ["language_server_php_cs_fixer.config"] = os.getenv("HOME") .. "/Developer/stack/utilities/php-cs-fixer/.php-cs-fixer.dist.php",
+        ["symfony.enabled"] = true,
     }
 }
 
@@ -65,9 +93,6 @@ cmp.setup({
         ['<Tab>'] = cmp.mapping.confirm({ select = true }),
         ['<C-Space>'] = cmp.mapping.complete(),
         ['<C-e>'] = cmp.mapping.abort(),
-        -- navigate between snippet placeholders
-        ['<C-f>'] = cmp_action.luasnip_jump_forward(),
-        ['<C-b>'] = cmp_action.luasnip_jump_backward(),
     }),
     formatting = lsp_zero.cmp_format({ details = true }),
 })
@@ -77,8 +102,13 @@ vim.g.copilot_assume_mapped = true
 
 vim.diagnostic.config({
     underline = true,
-    virtual_text = false,     -- Disable virtual text
+    virtual_text = false, -- Disable virtual text
     update_in_insert = false,
+    severity_sort = true,
+    float = {
+        show_header = true,
+        border = "single",
+    }
 })
 
 -- Show diagnostics in a floating window on hover
