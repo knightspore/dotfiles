5d3fcf8a74248651605fb0ed513088b5122ddb60
Ciarán <ciaran@tripco.africa>
2025-03-06 18:30:04 +0200

    feat(nvim): add dap config + don't gitignore

---
+++ .config/nvim/lua/plugins/dap.lua
@@ -0,0 +1,57 @@
+local dap = require('dap');
+
+dap.adapters.php = {
+    type = 'executable',
+    command = 'node',
+    args = { '/Users/c/Developer/tools/vscode-php-debug/out/phpDebug.js' }
+}
+
+dap.configurations.php = {
+    {
+        type = 'php',
+        request = 'launch',
+        name = 'Listen for Xdebug',
+        port = 9003,
+    }
+}
+
+dap.adapters.chrome = {
+    type = "executable",
+    command = "node",
+    args = {os.getenv("HOME") .. "/path/to/vscode-chrome-debug/out/src/chromeDebug.js"} -- TODO adjust
+}
+
+dap.configurations.javascriptreact = { -- change this to javascript if needed
+    {
+        type = "chrome",
+        request = "attach",
+        program = "${file}",
+        cwd = vim.fn.getcwd(),
+        sourceMaps = true,
+        protocol = "inspector",
+        port = 9222,
+        webRoot = "${workspaceFolder}"
+    }
+}
+
+dap.configurations.typescriptreact = { -- change to typescript if needed
+    {
+        type = "chrome",
+        request = "attach",
+        program = "${file}",
+        cwd = vim.fn.getcwd(),
+        sourceMaps = true,
+        protocol = "inspector",
+        port = 9222,
+        webRoot = "${workspaceFolder}"
+    }
+}
+
+require("nvim-dap-virtual-text").setup()
+
+require("mason-nvim-dap").setup()
+
+require("dapui").setup({
+    library = { "nvim-dap-ui" },
+})
+
